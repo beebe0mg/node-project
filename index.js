17e4a06796276401b44807a2e155393ba3d6e97f
정상미 <w2321@e-mirim.hs.kr>
2025-01-19 22:43:00 +0900

    figlet 모듈 사용해 보기

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+var figlet = require("figlet");
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+figlet("a3qua19", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
