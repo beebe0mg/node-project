dbfde47770f70844fba1746c1caae965be0660a8
정상미 <w2321@e-mirim.hs.kr>
2025-01-19 23:11:51 +0900

    json 으로 보내주기

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/dog', function (req, res) {
+  res.json({'sound' : '멍멍'})
+})
+
+app.get('/cat', function (req, res) {
+  res.send({'sound' : '야옹'})
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
