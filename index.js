32e2c47bc3c70e82d0c1d1ce81b901914ae77c4f
정상미 <w2321@e-mirim.hs.kr>
2025-01-19 23:24:33 +0900

    get 방식 이용해 보기

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/dog', function (req, res) {
-  res.json({'sound' : '멍멍'})
+app.get('/user/:id', function (req, res) {
+    // const q = req.params
+    // console.log(q.id);
+
+    const q = req.query
+    console.log(q.q);
+    console.log(q.name);
+    res.json({'userid' : q.name})
+
 })
 
 app.get('/cat', function (req, res) {
