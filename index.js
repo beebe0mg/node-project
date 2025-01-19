0463a3c40d7750b5f3255d71b066179152d290ab
정상미 <w2321@e-mirim.hs.kr>
2025-01-19 23:40:08 +0900

    내가 한 동물 소리 API 서버 만들기

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/user/:id', function (req, res) {
-    // const q = req.params
-    // console.log(q.id);
+// app.get('/user/:id', function (req, res) {
+//     // const q = req.params
+//     // console.log(q.id);
 
-    // const q = req.query
-    // console.log(q.q);
-    // console.log(q.name);
-    // res.json({'userid' : q.name})
+//     // const q = req.query
+//     // console.log(q.q);
+//     // console.log(q.name);
+//     // res.json({'userid' : q.name})
 
-    const p = req.params;
-    console.log(p);
-    const q = req.query;
-    console.log(q);
+//     const p = req.params;
+//     console.log(p);
+//     const q = req.query;
+//     console.log(q);
 
-    res.send({'message' : 'Hello World!'});
+//     res.send({'message' : 'Hello World!'});
+// })
+
+app.get('/sound/:name', function (req, res) {
+    const q = req.params
+
+    if(q.name == "cat") res.send('야옹')
+    if(q.name == "dog") res.send('멍멍')
+    if(q.name == "rabbit") res.send('깡총')
 })
 
-app.get('/cat', function (req, res) {
-  res.send({'sound' : '야옹'})
-})
-
-app.listen(port, () => {
+app.listen(port, () => { 
     console.log(`Example app listening on port ${port}`)
 })
