b65cb184889852d5c237de05ef46e10d681b478a
정상미 <w2321@e-mirim.hs.kr>
2025-01-26 22:37:44 +0900

    Mysql 과 연결하기ㅠㅠ

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const ejs = require('ejs')
+var mysql = require('mysql');
+const app = express()
+const port = 3000
+var bodyParser = require('body-parser')
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const cnn = mysql.createConnection({
+    host: 'localhost3306',
+    user: 'root',
+    password: '0000',
+    database: 'unidago'
+});
+
+app.get('/', (req, res) => {
+    res.render('example')
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+
+app.get('/map', (req, res) => {
+    res.render('map')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.post('/contactProc', (req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const memo = req.body.memo;
+
+    var a = ` ${name} ${phone} ${email} ${memo}`
+    res.send(a);
+})
+
+app.listen(port, () => {
+    console.log(`서버가 실행되었습니다. 접속 주소 : http://localhost:${port}`);
+})
