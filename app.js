80bf71e5619a9d9f83795e32b4dcfb40e5dd0208
정상미 <w2321@e-mirim.hs.kr>
2025-01-27 22:20:26 +0900

    Mysql2 설치와 연동 중. . .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
-var mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const app = express()
 const port = 3000
 var bodyParser = require('body-parser')
@@ -10,12 +10,40 @@ app.set('views', './views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const cnn = mysql.createConnection({
-    host: 'localhost3306',
-    user: 'root',
-    password: '0000',
-    database: 'unidago'
-});
+const connection = async () => {
+    try {
+      // MySQL 연결 설정 및 비동기 연결 생성
+      connection = await mysql.createConnection({
+        host: 'localhost3306',   // MySQL 서버의 주소
+        user: 'root',        // MySQL 사용자명
+        password: '0000', // MySQL 비밀번호
+        database: 'unidago'    // 사용할 데이터베이스 이름
+      });
+  
+      console.log('MySQL에 성공적으로 연결되었습니다.');
+    } catch (err) {
+      console.error('MySQL 연결 실패:', err.stack);
+    }
+  };
+
+  // 연결 종료 함수
+const closeConnection = async () => {
+    if (connection) {
+      try {
+        await connection.end();
+        console.log('MySQL 연결이 성공적으로 종료되었습니다.');
+      } catch (err) {
+        console.error('연결 종료 중 오류가 발생했습니다:', err.stack);
+      }
+    }
+  };
+
+(async () => {
+	// 연결 시도
+  await connectToDatabase();
+  // 연결 종료
+  await closeConnection();
+})();
 
 app.get('/', (req, res) => {
     res.render('example')
